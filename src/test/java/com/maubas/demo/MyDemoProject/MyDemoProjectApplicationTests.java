a20b2e2ec27b5ce402a0535c2500e4b45a5ec4e9
mauricio-araujoo <mauriciofearatrabalho@gmail.com>
2025-05-19 14:00:40 -0300

    add: first commit

---
+++ src/test/java/com/maubas/demo/MyDemoProject/MyDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maubas.demo.MyDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
